7e18e69020b8457d65d86f3b9c6d875bb96aa447
aakashdeveloper <ahanda205@gmail.com>
2023-03-24 08:48:38 +0530

    fsall

--- inbuilt/fs.js
+++ inbuilt/fs.js
@@ -1,6 +1,34 @@
 let fs = require('fs');
 
-fs.writeFile('mycode.txt','We are using express',(err)=>{
+// fs.writeFile('mycode.txt','We are using express',(err)=>{
+//     if(err) throw err;
+//     console.log('Task Done')
+// })
+
+// fs.appendFile('myText.txt','Code from Node FS \n',(err) => {
+//     if(err) throw err;
+//     console.log('File appended')
+// })
+
+// fs.readFiles('myText.txt','utf-8',(err,data)=>{
+//     if(err) throw err;
+//     console.log(data)
+// })
+
+
+// let data  = fs.readFileSync('city.json',{encoding:'utf-8',flag:'r'})
+// console.log(data)
+
+// let data1  = fs.readFileSync('myText.txt',{encoding:'utf-8',flag:'r'})
+// console.log(data1)
+
+
+// fs.unlink('mycode.txt',(err)=>{
+//     if(err) throw err;
+//     console.log('file deleted')
+// })
+
+fs.rename('myText.txt','myFile.xml',(err)=>{
     if(err) throw err;
-    console.log('Task Done')
+    console.log('file renamed')
 })
