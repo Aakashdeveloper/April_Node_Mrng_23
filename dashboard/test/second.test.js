517eb77675acfbbcc22cbbad4c5520e7f9304e3e
aakashdeveloper <ahanda205@gmail.com>
2023-05-01 08:57:49 +0530

    jenkins

--- dashboard/test/second.test.js
+++ dashboard/test/second.test.js
@@ -15,4 +15,27 @@ describe("Users Testing Api",() => {
             throw err
         })
     })
+    it("Should return 404 for the user",(done) => {
+        chai.request('http://localhost:7710')
+        .get('/user')
+        .then((res) => {
+            expect(res).to.have.status(404)
+            done()
+        })
+        .catch((err) =>{
+            throw err
+        })
+    })
+    it("Should return 200 for the addUser",(done) => {
+        chai.request('http://localhost:7710')
+        .post('/addUser')
+        .send({"name":"Test User","City":"Test CIty","isActive":false})
+        .then((res) => {
+            expect(res).to.have.status(200)
+            done()
+        })
+        .catch((err) =>{
+            throw err
+        })
+    })
 })
